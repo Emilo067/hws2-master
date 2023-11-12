649820e6ded83438a37e78f2b8de29c1725d31aa
Emilo067 <gasanovemil467@gmail.com>
2023-11-12 12:01:38 +0300

    HomeTask 5

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -27,11 +27,16 @@ function Affairs(props: AffairsPropsType) {
         // need to fix
         props.setFilter("low")
     }
-console.log(props.filter,s)
-    const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? + ' ' + s.active : '')
-    const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ?  + ' ' + s.active : '')
-    const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ?  + ' ' + s.active : '')
-    const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ?  + ' ' + s.active : '')
+
+    const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ?  ' ' + s.active : '')
+    const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ?   ' ' + s.active : '')
+    const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ?   ' ' + s.active : '')
+    const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ?   ' ' + s.active : '')
+
+    // const cnAll = `${s.button} ${s.all} ${props.filter === 'all' ? s.active : ''}`
+    // const cnHigh = `${s.button} ${s.high} ${props.filter === 'high' ? s.active : ''}`
+    // const cnMiddle = `${s.button} ${s.middle} ${props.filter === 'middle' ? s.active : ''}`
+    // const cnLow = `${s.button} ${s.low} ${props.filter === 'low' ? s.active : ''}`
 
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
