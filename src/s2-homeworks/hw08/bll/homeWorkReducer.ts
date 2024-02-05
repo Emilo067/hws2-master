58b4c31d74044e9aeab0c9bb67d8fd795c60cb7a
Emilo067 <gasanovemil467@gmail.com>
2024-02-05 22:19:13 +0300

    HomeTask 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if(action.payload === 'up') {
+                const sortedState = state.slice().sort((a, b) => a.name.localeCompare(b.name));
+                return sortedState // need to fix
+            } else {
+                const sortedState = state.slice().sort((a, b) => b.name.localeCompare(a.name));
+                return sortedState
+            }
         }
         case 'check': {
 
-            return state // need to fix
+            return  state.filter(u => u.age >= 18)// need to fix
         }
         default:
             return state
